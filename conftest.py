f8f3433e5166471438b05dfd431fab8ae2530838
LaenorSama <laenorsama@yandex.ru>
2025-08-29 14:50:10 +0500

    lucky_step 60%

--- conftest.py
+++ conftest.py
@@ -3,20 +3,37 @@ import os
 import pytest
 from func import *  # импортируем твою функцию
 
-@pytest.fixture(autouse=True)
-def apply_lucky_sleep_conditionally():
+# фикстура запускается перед каждым тестом, если в воркфлоу указаны параметры задержки
+# @pytest.fixture(autouse=True)
+# def apply_lucky_sleep_conditionally():
+#     """
+#     Выполняется только если заданы переменные окружения LUCKY_SLEEP_MIN и LUCKY_SLEEP_MAX.
+#     """
+#     min_env = os.getenv("LUCKY_SLEEP_MIN")
+#     max_env = os.getenv("LUCKY_SLEEP_MAX")
+#
+#     # Если хотя бы одна из переменных не задана, фикстура пропускается
+#     if min_env is None or max_env is None:
+#         return
+#
+#     # Преобразуем в int
+#     min_seconds = int(min_env)
+#     max_seconds = int(max_env)
+#
+#     lucky_sleep(min_seconds, max_seconds)
+
+# хук который запускается прямо внутри теста если в воркфлоу е сть параметры
+def pytest_runtest_call(item):
     """
-    Выполняется только если заданы переменные окружения LUCKY_SLEEP_MIN и LUCKY_SLEEP_MAX.
+    Хук выполняется **внутри каждого теста**, перед его основной логикой.
     """
     min_env = os.getenv("LUCKY_SLEEP_MIN")
     max_env = os.getenv("LUCKY_SLEEP_MAX")
 
-    # Если хотя бы одна из переменных не задана, фикстура пропускается
     if min_env is None or max_env is None:
-        return
+        return  # ничего не делаем, тест выполняется сразу
 
-    # Преобразуем в int
     min_seconds = int(min_env)
     max_seconds = int(max_env)
 
-    lucky_sleep(min_seconds, max_seconds)
+    lucky_sleep(min_seconds, max_seconds)  # тест реально ждёт здесь
